b27c14f9b7d72bdf7b139dbfde17468b5778c2a4
Ryo-oz <al23097@shibaura-it.ac.jp>
2025-06-17 15:42:21 +0900

    メイン画面追加

---
+++ frontend/src/app/Input/page.tsx
@@ -0,0 +1,10 @@
+import TripInputForm from "@/components/Input/TripInputForm";
+import React from "react";
+export default function InputPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">Inputページです</h1>
+      <TripInputForm />
+    </div>
+  );
+}
