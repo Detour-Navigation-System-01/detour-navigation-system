f599183d717e28c9ec8888a027d3ca461a98135f
keigo <keigomaneki0119@gmail.com>
2025-07-05 13:01:09 +0900

    fix(0705)

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,8 +3,8 @@
  * @description ユーザー認証のためのログインフォームを表示するページコンポーネント
  * @author 平野
  * @created 2025-06-17
- * @updated 2025-06-24
- * @version 2.0.0
+ * @updated 2025-07-05
+ * @version 2.1.0
  */
 
 import LoginForm from '@/components/auth/LoginForm';
@@ -12,7 +12,6 @@ import LoginForm from '@/components/auth/LoginForm';
 export default function LoginPage() {
   return (
     <main>
-      <h1>ログインページ</h1>
       <LoginForm />
     </main>
   );
